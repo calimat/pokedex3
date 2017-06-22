5d575581d60232ff3516bbb7df8f0d8609b01c9c
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-06-21 21:31:07 -0500

    changes for the ui.

--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+    
     var pokemon:Pokemon!
     
+    
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var currentEvoIMG: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         nameLbl.text = pokemon.name
         
     }
-
-
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
